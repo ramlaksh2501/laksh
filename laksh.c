cd74f7d0a423aa8f66e79d851318d5fa1b6effe7
ramlaksh2501 <ramlaksh2501@gmail.com>
2026-09-13 20:13:14 +0530

    feat: exit: to exit the shell

--- laksh.c
+++ laksh.c
@@ -1,18 +1,22 @@
 #include<stdio.h>
 #include<unistd.h>
 #include<sys/wait.h>
+#include<string.h>
 
 int main(int argv, char ** argc){
 	int i=0;
 	while(1){
-	int id=fork();
-	if(id==0){
-
+	
 	printf(">");
 	char input[1000];
 	scanf("%s",input);
 	printf("%s\n",input);
-		execlp(input,NULL);
+	input[strcspn(input,"\n")]=0;
+	if(strcmp(input,"exit")==0) return 0;
+	int id=fork();
+	if(id==0){
+	   char args[]=" hello.txt";
+	   execlp(input,args,(char *)NULL);
 	}
 	wait(NULL);
 	printf("job done\n");
