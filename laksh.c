e18f08e9055b0cd126d1b3dadb426d59aa40a525
ramlaksh2501 <ramlaksh2501@gmail.com>
2026-09-14 23:10:47 +0530

    add: error handle: invalid cmds
    invalid cmds cause the program to multiply as there is no exit value for failed exec , so the child process forked itself and the processes multiplied everytime a invalid cmd is passed now its fixed

--- laksh.c
+++ laksh.c
@@ -14,13 +14,16 @@ int main(int argv, char ** argc){
 	printf(">");
 	char input[MAX_INPUT];
 	fgets(input,MAX_INPUT,stdin);
-	printf("%s\n",input);
 	input[strcspn(input,"\n")]=0;
-	char **token=tokenize(input);
+	printf("%s\n",input);
 	if(strcmp(input,"exit")==0) return 0;
 	int id=fork();
 	if(id==0){
-	   execvp(token[0],token);
+		char **token=tokenize(input);
+		if(execvp(token[0],token)<0){
+			printf("No such cmd as \"%s\"\n",token[0]);
+			return 0;
+			}
 	}
 	wait(NULL);
 	printf("job done\n");
