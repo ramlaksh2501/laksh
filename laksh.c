8005fd3412033c99d1bc1620d5697573a6f7ef02
ramlaksh2501 <ramlaksh2501@gmail.com>
2026-09-14 22:05:49 +0530

    feat: tokenize input: to pass argument
    need to be tested

--- laksh.c
+++ laksh.c
@@ -2,21 +2,24 @@
 #include<unistd.h>
 #include<sys/wait.h>
 #include<string.h>
+#include<stdlib.h>
+
+
+#define MAX_INPUT 1000
 
 int main(int argv, char ** argc){
 	int i=0;
 	while(1){
-	
 	printf(">");
-	char input[1000];
+	char input[MAX_INPUT];
 	scanf("%s",input);
 	printf("%s\n",input);
 	input[strcspn(input,"\n")]=0;
+	char **token=tokenize(input);
 	if(strcmp(input,"exit")==0) return 0;
 	int id=fork();
 	if(id==0){
-	   char args[]=" hello.txt";
-	   execlp(input,args,(char *)NULL);
+	   execvp(token[0],token);
 	}
 	wait(NULL);
 	printf("job done\n");
