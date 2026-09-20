5304e421a2d6cbe980208be89a5c79a49384e285
ramlaksh2501 <ramlaksh2501@gmail.com>
2026-09-21 00:42:09 +0530

    "feat: handling double quotes: works"

--- tokenizer.c
+++ tokenizer.c
@@ -1,6 +1,7 @@
 #include<stddef.h>
 #include<stdlib.h>
 #include<string.h>
+#include"tokenize.h"
 extern char** tokenize(char *input){
 	char *copy=malloc(sizeof(char)*strlen(input)+1);
 	strcpy(copy,input);
@@ -13,7 +14,45 @@ extern char** tokenize(char *input){
 	char **tokens=(char**)malloc(sizeof(char*)*tok_count+1);
 	tok=(char*)strtok(copy," ");
 	int i=0;
+	int got_quotes=0;
+	char *qoute_tok;
 	while(tok!=NULL){
+		if(tok[0]=='\"'){
+			got_quotes=1;
+			if(tok[strlen(tok)-1]=='\"'){
+				got_quotes=0;
+			}
+			else{
+				qoute_tok=malloc(sizeof(char)*strlen(tok)+2);
+				qoute_tok=strcpy(qoute_tok,tok);
+				strcat(qoute_tok," ");
+				tok=strtok(NULL,"\"");
+				if(tok!=NULL){
+					qoute_tok=realloc(qoute_tok,sizeof(char)*(strlen(tok)+strlen(qoute_tok)+1));
+					strcat(qoute_tok,tok);
+					strcat(qoute_tok,"\"");
+					tokens[i]=(char*)malloc(sizeof(char)*strlen(qoute_tok)+1);
+					strcpy(tokens[i],qoute_tok);
+					tok=(char*)strtok(NULL," ");
+					free(qoute_tok);
+					i++;
+					continue;
+				}
+				else{
+					return ERR_ENDQOT;
+				}
+
+
+			}
+			}
+		
+		//	if else(got_qoutes && tok[1]!=NULL && (tok[1][strlen(tok[1])-1]!='\"')){
+		//		strcat(tok[0],tok[1]);
+		//	}
+		//	if else(got_qoutes && tok[1]!=NULL && (tok[1][strlen(tok[1])-1]=='\"')){
+		//		strcat(tok[0],tok[1]);
+		//		got_qoutes=0;
+		//	}
 		tokens[i]=(char*)malloc(sizeof(char)*strlen(tok)+1);
 		strcpy(tokens[i],tok);
 		tok=(char*)strtok(NULL," ");
